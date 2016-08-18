4876ccb961e35474ae9c699f830ab9329d174315
Robbie Parks <rmiparks@Robbie-Parkss-Big-MacBook-Pro.local>
2016-08-18 21:34:37 +0100

    added files

--- countries/USA/prog/01_extract_netcdf/extracting_netcdf_files.R
+++ countries/USA/prog/01_extract_netcdf/extracting_netcdf_files.R
@@ -9,6 +9,7 @@ rm(list=ls())
 library(RColorBrewer)
 library(lattice)
 library(ncdf4)
+library(lubridate)
 
 # year of interest
 year <- 1982
@@ -18,14 +19,18 @@ year <- 1982
 #ncname <- 'worldwide_monthly.nc'
 # daily data
 dname <- 't2m'
-ncname <- paste0('worldwide_',dname,'_',year,'.nc')
+freq <- 'daily'
+num <- 'twice'
+ncname <- paste0('worldwide_',dname,'_',freq,'_',num,'_',year,'.nc')
 
 year <- unlist(strsplit(ncname, "_"))
-year <- year[3]
+year <- year[5]
 year <- unlist(strsplit(year, ".nc"))
 
 # open NetCDF file
-ncin <- nc_open(paste0('../../data/netcdf/',dname,'/',ncname))
+ncin <- nc_open(paste0('../../data/net_cdf/',dname,'/',ncname))
+#"../../data/net_cdf/t2m/worldwide_t2m_daily_twice_1982.nc"
+#'../../data/net_cdf/t2m/worldwide_2tm_daily_twice_1982.nc'
 
 # get long and lat data
 lon <- ncvar_get(ncin, 'longitude')
@@ -70,7 +75,7 @@ t.names <- as.POSIXct(t.sec, origin = "1900-01-01")
 timeStamp <-  strptime(t.names,"%Y-%m-%d %H:%M:%S")
 
 # round to days
-t.names <- as.character(round(timeStamp, 'days'))
+t.names <- as.character(floor_date(timeStamp, 'day'))
 
 # convert tmp.array into long file
 tmp.vec.long <- as.vector(tmp.array)
@@ -83,36 +88,19 @@ dim(tmp.mat)
 # create second data frame
 lonlat <- expand.grid(lon, lat)
 tmp.df02 <- data.frame(cbind(lonlat, tmp.mat)) ## important line
-names(tmp.df02) <- c('long','lat',t.names)
+names(tmp.df02) <- c('lon','lat',t.names)
 head(colnames(tmp.df02))
 
 # Fixing longitude values so range is -180 to 180, not 0 to 360
-global_lon_lat.df = read.csv("../../data/lon_lat/global_lon_lat.csv", header=TRUE)
-tmp.df02$long <- global_lon_lat.df$long
-tmp.df02$lat <- global_lon_lat.df$lat
+dat <- as.data.frame(tmp.df02)
+dat$lon <- dat$lon - 180
 
-# Clipping off last row because it starts the next year
-end <- ncol(tmp.df02)
-tmp.df02[end] <- NULL
+# write to rds file with naming according to year
+ifelse(!dir.exists("../../output/extracting_netcdf_files"), dir.create("../../output/extracting_netcdf_files"), FALSE)
+file.name <- paste0('../../output/extracting_netcdf_files/','worldwide_',dname,'_',freq,'_',num,'_',year,'.rds')
+saveRDS(dat, file=file.name)
 
-# write to csv file with naming according to year
-csv.name <- paste0('../../data/csv/',"worldwide_t2m_", year, ".csv")
-write.csv(tmp.df02, file=csv.name)
-
-rm(tmp.array)
-rm(tmp.df02)
-rm(tmp.mat)
-rm(tmp.vec.long)
-rm(lat)
-rm(lon)
-
-#########
-
-# Col names after reading this back in were like "X1982.01.02" "X1982.01.02.1"
-year.df <- read.csv(csv.name, nrows=5) # Use for loop to cycle through names?
-names(year.df) <- c('long','lat',t.names[1:3])
-
-# extract csv file of latitude longitude for USA
+# extract csv file of latitude longitude for USA if required
 #USA.lonlat <- lonlat
 #names(USA.lonlat) <- c('lon','lat')
 #USA.lonlat$lon <- as.numeric(USA.lonlat$lon)
