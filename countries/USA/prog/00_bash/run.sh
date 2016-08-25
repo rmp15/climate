298e5623affa86bcb1f55a77b93b2d71ea5e7fb9
Robbie Parks <rmiparks@Robbie-Parkss-Big-MacBook-Pro.local>
2016-08-25 21:45:19 +0100

    added multiple months for comparison

--- countries/USA/prog/00_bash/run.sh
+++ countries/USA/prog/00_bash/run.sh
@@ -4,7 +4,7 @@
 
 #Rscript ~/git/climate/countries/USA/prog/03_population_weighted_mean/population_weighted_mean.R
 
-for year in $(seq 1982 1983);
+for year in $(seq 1982);
 
 do 
 
@@ -16,10 +16,21 @@ echo $year;
 
 #Rscript ~/git/climate/countries/USA/prog/05_plots/plots.R $year
 
-for month in $(seq 2 1);
+#for month in $(seq 2 12);
 
-Rscript ~/git/climate/countries/USA/prog/06_bil_files/bil_files.R $month $year
+Rscript ~/git/climate/countries/USA/prog/06_bil_files/bil_files.R 2 $year
+Rscript ~/git/climate/countries/USA/prog/06_bil_files/bil_files.R 3 $year
+Rscript ~/git/climate/countries/USA/prog/06_bil_files/bil_files.R 4 $year
+Rscript ~/git/climate/countries/USA/prog/06_bil_files/bil_files.R 5 $year
+Rscript ~/git/climate/countries/USA/prog/06_bil_files/bil_files.R 6 $year
+Rscript ~/git/climate/countries/USA/prog/06_bil_files/bil_files.R 7 $year
+Rscript ~/git/climate/countries/USA/prog/06_bil_files/bil_files.R 8 $year
+Rscript ~/git/climate/countries/USA/prog/06_bil_files/bil_files.R 9 $year
+Rscript ~/git/climate/countries/USA/prog/06_bil_files/bil_files.R 10 $year
+Rscript ~/git/climate/countries/USA/prog/06_bil_files/bil_files.R 11 $year
+Rscript ~/git/climate/countries/USA/prog/06_bil_files/bil_files.R 12 $year
 
-done
+
+#done
 
 done
