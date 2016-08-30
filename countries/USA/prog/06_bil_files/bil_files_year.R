3c20d3ec5c963a35cc13350dc2b21aa330314153
Robbie Parks <rmiparks@dyn1177-38.wlan.ic.ac.uk>
2016-08-30 12:14:15 +0100

    added colour

--- countries/USA/prog/06_bil_files/bil_files_year.R
+++ countries/USA/prog/06_bil_files/bil_files_year.R
@@ -1,23 +1,14 @@
 rm(list=ls())
 
-library(maptools)
-library(mapproj)
-library(rgeos)
-library(rgdal)
-library(RColorBrewer)
 library(ggplot2)
-library(raster)
-library(sp)
-library(plyr)
-library(graticule)
-library(spatialEco)
-library(reshape)
+library(RColorBrewer)
 
 # arguments from Rscript
 year <- as.character(args[1])
 
 # list of months for file name loading
 month <- as.character(c('01','02','03','04','05','06','07','08','09','10','11','12'))
+month.short <- c('Jan','Feb','Mar','Apr','May','Jun','Jul','Aug','Sep','Oct','Nov','Dec')
 
 # load the data
 dat.merged <- data.frame()
@@ -36,15 +27,11 @@ print(paste0('R squared value is ',round(r.squared,2)))
 # plot with line of best fit overlaid
 pdf(paste0("../../output/bil_files/era_prism_correlation_all_months_",year,'.pdf'))
 ggplot() +
-geom_point(data=dat.merged,aes(x=prism.temp,y=era.temp)) +
+geom_point(data=dat.merged,aes(color=as.factor(month),x=prism.temp,y=era.temp)) +
 geom_abline(intercept=coef(fit)[1],slope=coef(fit)[2],color='red') +
 ggtitle(paste0('ERA-Interim derived values against PRISM derived values ',year,':\n R^2=',round(r.squared,2))) +
+scale_colour_manual(values=colorRampPalette(rev(brewer.pal(12,"RdYlBu")[c(9:10,2:1,1:2,10:9)]))(12),guide = guide_legend(title = 'month'),labels=month.short) +
 xlab('PRISM Temperature') +
 ylab('ERA Temperature') +
 theme_bw()
 dev.off()
-
-# overlay grid on raster map if required
-#plot(grat.poly);plot(dat.prism,add=1)
-# and test to add highlight grid square in blue
-#plot(grat.poly[grat.poly@data$layer==7938,],border='blue',lwd=2,add=TRUE)
