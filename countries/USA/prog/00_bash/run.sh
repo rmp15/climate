41addd4d62b2799659eb2bf005f3da6f9eb72282
Robbie Parks <r.parks15@imperial.ac.uk>
2016-08-31 12:23:11 +0100

    tidying

--- countries/USA/prog/00_bash/run.sh
+++ countries/USA/prog/00_bash/run.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
 
+#################################################
+# 1. WEIGHTED MEAN PREPARATION
+#################################################
+
+# identifies the overlap between grids and counties and creates weighted means
 #Rscript ~/git/climate/countries/USA/prog/02_grid_county_intersection/grid_county_intersection_unproj_function.R
 
+# creates a weighted mean per state based on county populations
 #Rscript ~/git/climate/countries/USA/prog/03_population_weighted_mean/population_weighted_mean.R
 
 for year in $(seq 1982 1983);
@@ -10,7 +16,10 @@ do
 
 echo $year;
 
+# processes net_cdf files
 #Rscript ~/git/climate/countries/USA/prog/01_extract_netcdf/extracting_netcdf_files.R $year
+
+# 
 
 Rscript ~/git/climate/countries/USA/prog/04_state_weighted_mean_summary/state_weighted_mean_summary.R $year
 
