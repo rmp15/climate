79aaaf68266838f162a0a70eafe5ec254873f122
Robbie Parks <rmiparks@dyn1180-193.wlan.ic.ac.uk>
2016-08-31 10:29:22 +0100

    added test files

--- countries/USA/prog/04_state_weighted_mean_summary/metrics_development.R
+++ countries/USA/prog/04_state_weighted_mean_summary/metrics_development.R
@@ -7,12 +7,12 @@ library(lubridate)
 
 # arguments from Rscript
 args <- commandArgs(trailingOnly=TRUE)
+year <- as.numeric(args[1])
 
 ifelse(!dir.exists("../../output/state_weighted_mean_summary"), dir.create("../../output/state_weighted_mean_summary"), FALSE)
 
 # for each county, load summary by day and then by month (for single year)
-# NEED A DAILY NOT MONTHLY FILE
-dat.county <- readRDS(paste0('../../output/state_weighted_mean_summary/county_summary_',year,'.rds'))
+dat.county <- readRDS(paste0('../../output/state_weighted_mean_summary/county_daily_',year,'.rds'))
 
 # load weightings by county for state summary based on population
 state.weighting <- readRDS('~/data/climate/population_weightings/state_population_weightings.rds')
@@ -21,7 +21,13 @@ state.weighting <- readRDS('~/data/climate/population_weightings/state_populatio
 year.selected <- year
 state.weighting.filter <- subset(state.weighting,year %in% year.selected)
 
-dat.temp <-merge(dat.wm,state.weighting.filter,by=c('year','month','state.county.fips'))
+# 1. NUMBER OF DAYS ABOVE A THRESHOLD
+var <- 'no_above_threshold'
+
+# CREATE PROCESS FOR dat.th
+
+# merge and create weighted mean for state
+dat.temp <-merge(dat.th,state.weighting.filter,by=c('year','month','state.county.fips'))
 temp.state <- ddply(dat.temp,.(year,month,state.fips,sex,age),summarize,temp.adj=sum(pop.weighted*temp.weighted))
 temp.state <- na.omit(temp.state)
 
@@ -29,5 +35,5 @@ temp.state <- na.omit(temp.state)
 temp.state$temp.cel <- temp.state$temp.adj- 273.15
 
 # save output
-saveRDS(dat.wm,paste0('../../output/state_weighted_mean_summary/county_summary_',year.selected,'.rds'))
-saveRDS(temp.state,paste0('../../output/state_weighted_mean_summary/state_weighted_summary_',year.selected,'.rds'))
+saveRDS(dat.wm,paste0('../../output/state_weighted_mean_summary/county_summary_',var,'_',year.selected,'.rds'))
+saveRDS(temp.state,paste0('../../output/state_weighted_mean_summary/state_weighted_summary_',var,'_',year.selected,'.rds'))
