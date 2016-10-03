d067cde79ba24f33a41fdce486254d0674cac913
Robbie Parks <r.parks15@imperial.ac.uk>
2016-10-03 17:25:42 +0100

    changes

--- countries/USA/prog/00_bash/metric_create.sh
+++ countries/USA/prog/00_bash/metric_create.sh
@@ -6,7 +6,7 @@
 
 clear
 
-declare -a years=($(seq 1982 2013))
+declare -a years=($(seq 1983 1991))
 
 #################################################
 # 1. 2-METRE TEMPERATURE PROCESSING (t2m)
@@ -19,29 +19,33 @@ echo "processing temperature variables for $year";
 echo "converting temperature netcdf file for $year";
 
 # processes net_cdf files
-#Rscript ~/git/climate/countries/USA/prog/01_extract_netcdf/extracting_netcdf_files.R $year t2m daily four
+Rscript ~/git/climate/countries/USA/prog/01_extract_netcdf/extracting_netcdf_files.R $year t2m daily four
 
 echo "creating temperature metric for counties for $year";
 
 # creates a weighted mean from grid county intersection of temperature per day per county for year 
-#Rscript ~/git/climate/countries/USA/prog/04_county_weighted_mean_summary/county_weighted_mean_summary.R $year t2m daily four
+Rscript ~/git/climate/countries/USA/prog/04_county_weighted_mean_summary/county_weighted_mean_summary.R $year t2m daily four
 
 echo "creating temperature metric for states for $year";
 
 # creates metrics from the temperature values processed
-#Rscript ~/git/climate/countries/USA/prog/05_metrics_development/metrics_development.R $year t2m
+# NEEDS TO OUTPUT VARIABLES TO A LOCATION WHERE THEY CAN BE READ BY THE PLOTTING FUNCTION
+Rscript ~/git/climate/countries/USA/prog/05_metrics_development/metrics_development.R $year t2m
+
+# bind together data for each year
+# SCRIPT TO BIND DATA OVER ALL VARIABLES
 
 # plots
 
+# SEE ABOVE ABOUT LOCATION OF VARIABLES FOR PLOTTING FUNCTION
 declare -a metrics=("mean" "days_below_10" "days_above_30" "sd" "days_changing_by_5" "days_increasing_by_5" "days_decreasing_by_5" "number_of_min_3_day_above_25_upwaves" "number_of_min_3_day_below_5_downwaves")
 
 echo "plotting temperature metric results for $year";
 
 for metric in "${metrics[@]}"; do
 
-# delete this line when up and running
-:
-#Rscript ~/git/climate/countries/USA/prog/06_plots/plots.R $year t2m $metric
+#:
+Rscript ~/git/climate/countries/USA/prog/06_plots/plots.R $year t2m $metric
 
 done
 
