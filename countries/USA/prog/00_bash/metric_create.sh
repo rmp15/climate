30cadcca89d138e3ada4a73cb43f6e99561f9eb9
Robbie Parks <r.parks15@imperial.ac.uk>
2016-10-10 17:39:18 +0100

    cleaned up

--- countries/USA/prog/00_bash/metric_create.sh
+++ countries/USA/prog/00_bash/metric_create.sh
@@ -23,18 +23,18 @@ echo "processing temperature variables for $year";
 echo "converting temperature netcdf file for $year";
 
 # processes net_cdf files
-#Rscript ~/git/climate/countries/USA/prog/01_extract_netcdf/extracting_netcdf_files.R $year $dname $freq $num
+Rscript ~/git/climate/countries/USA/prog/01_extract_netcdf/extracting_netcdf_files.R $year $dname $freq $num
 
 echo "creating temperature metric for counties for $year";
 
 # creates a weighted mean from grid county intersection of temperature per day per county for year 
-#Rscript ~/git/climate/countries/USA/prog/04_county_weighted_mean_summary/county_weighted_mean_summary.R $year $dname $freq $num
+Rscript ~/git/climate/countries/USA/prog/04_county_weighted_mean_summary/county_weighted_mean_summary.R $year $dname $freq $num
 
 echo "creating temperature metric for states for $year";
 
 # creates metrics from the temperature values processed
 # NEEDS TO OUTPUT VARIABLES TO A LOCATION WHERE THEY CAN BE READ BY THE PLOTTING FUNCTION
-#Rscript ~/git/climate/countries/USA/prog/05_metrics_development/metrics_development.R $year $dname
+Rscript ~/git/climate/countries/USA/prog/05_metrics_development/metrics_development.R $year $dname
 
 # SEE ABOVE ABOUT LOCATION OF VARIABLES FOR PLOTTING FUNCTION
 declare -a metrics=("mean" "days_below_10" "days_above_30" "sd" "days_changing_by_5" "days_increasing_by_5" "days_decreasing_by_5" "number_of_min_3_day_above_25_upwaves" "number_of_min_3_day_below_5_downwaves")
@@ -70,18 +70,18 @@ echo "processing temperature variables for $year";
 echo "converting temperature netcdf file for $year";
 
 # processes net_cdf files
-#Rscript ~/git/climate/countries/USA/prog/01_extract_netcdf/extracting_netcdf_files.R $year $dname $freq $num
+Rscript ~/git/climate/countries/USA/prog/01_extract_netcdf/extracting_netcdf_files.R $year $dname $freq $num
 
 echo "creating temperature metric for counties for $year";
 
 # creates a weighted mean from grid county intersection of temperature per day per county for year
-#Rscript ~/git/climate/countries/USA/prog/04_county_weighted_mean_summary/county_weighted_mean_summary.R $year $dname $freq $num
+Rscript ~/git/climate/countries/USA/prog/04_county_weighted_mean_summary/county_weighted_mean_summary.R $year $dname $freq $num
 
 echo "creating temperature metric for states for $year";
 
 # creates metrics from the temperature values processed
 # NEEDS TO OUTPUT VARIABLES TO A LOCATION WHERE THEY CAN BE READ BY THE PLOTTING FUNCTION
-#Rscript ~/git/climate/countries/USA/prog/05_metrics_development/metrics_development.R $year $dname
+Rscript ~/git/climate/countries/USA/prog/05_metrics_development/metrics_development.R $year $dname
 
 # SEE ABOVE ABOUT LOCATION OF VARIABLES FOR PLOTTING FUNCTION
 declare -a metrics=("mean" "days_below_10" "days_above_30" "sd" "days_changing_by_5" "days_increasing_by_5" "days_decreasing_by_5" "number_of_min_3_day_above_25_upwaves" "number_of_min_3_day_below_5_downwaves")
@@ -91,7 +91,7 @@ echo "plotting temperature metric results for $year";
 for metric in "${metrics[@]}"; do
 
 # plots
-#Rscript ~/git/climate/countries/USA/prog/06_plots/plots.R $year $dname $metric
+Rscript ~/git/climate/countries/USA/prog/06_plots/plots.R $year $dname $metric
 
 done; 
 
@@ -101,6 +101,6 @@ for metric in "${metrics[@]}"; do
 
 :
 # bind together data for each variable
-#Rscript ~/git/climate/countries/USA/prog/08_bind_results/bind_results.R ${years[0]} ${years[-1]} $dname $metric
+Rscript ~/git/climate/countries/USA/prog/08_bind_results/bind_results.R ${years[0]} ${years[-1]} $dname $metric
 
 done;
