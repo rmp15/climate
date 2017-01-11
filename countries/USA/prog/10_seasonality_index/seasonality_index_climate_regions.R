d84fc8abc7aef82c5bbea2e6cbc7d17fac2db4dd
Robbie Parks <rmiparks@dyn1176-83.wlan.ic.ac.uk>
2017-01-11 10:55:39 +0000

    added stuff

--- countries/USA/prog/10_seasonality_index/seasonality_index_climate_regions.R
+++ countries/USA/prog/10_seasonality_index/seasonality_index_climate_regions.R
@@ -40,6 +40,41 @@ if(dname=='t2m'){
 
 # 1. CLIMATE REGION
 
+# STATIC MAX MIN DEFINED BY COM
+
+# load com data to establish max min locations
+file.loc.nat.input <- paste0("~/git/mortality//USA/state/output/com/",year.start,'_',year.end,"/national/values/combined_results/")
+dat.COM <- readRDS(paste0(file.loc.nat.input,'com_inv_com_national_values_method_2_entire_',year.start,'_',year.end))
+
+# round to get month required for merging
+dat.COM$COM.mean <- round(dat.COM$COM.mean)
+dat.COM$COM.mean <- ifelse(dat.COM$COM.mean==0,12,dat.COM$COM.mean)
+dat.COM$month <- dat.COM$COM.mean
+levels(dat.COM$sex) <- c(1,2)
+
+# figure out the ratio of max/min temperature over time with fixed max/min by sex, age, year
+dat.max.min.fixed <- merge(dat,dat.COM,by=c('age','sex','month'))
+dat.max.min.fixed <- ddply(dat.max.min.fixed,.(sex,age,year,climate_region), summarize,max=variable[type=='max'],month.max=month[type=='max'],min=variable[type=='min'],month.min=month[type=='min'])
+dat.max.min.fixed$ratio <- with(dat.max.min.fixed,abs(max-min)/abs(min))
+dat.max.min.fixed$percent.change <- round(100*(dat.max.min.fixed$ratio),1)
+
+# add time value that starts at 0
+dat.max.min.fixed$year.centre <- with(dat.max.min.fixed,year-year.start)
+
+# apply linear regression to each group by sex, age, month to find gradient
+lin.reg.grad <- ddply(dat.max.min.fixed, .(sex,age,climate_region), function(z)coef(lm(percent.change ~ year.centre, data=z)))
+lin.reg.grad$end.value <- with(lin.reg.grad,`(Intercept)`+year.centre*(num.years-1))
+lin.reg.grad$start.value <- lin.reg.grad$`(Intercept)`
+
+# obtain significance of slopes
+lin.reg.sig <- ddply(dat.max.min.fixed, .(sex,age,climate_region), function(z)coef(summary(lm(percent.change ~ year.centre, data=z))))
+lin.reg.sig <- lin.reg.sig[!c(TRUE,FALSE),]
+lin.reg.sig$sig.test.10 <- ifelse(lin.reg.sig[,7]<0.10,1,0)
+lin.reg.sig$sig.test.5 <- ifelse(lin.reg.sig[,7]<0.05,1,0)
+
+# merge with data about gradients
+lin.reg.grad <- merge(lin.reg.grad,lin.reg.sig,by=c('sex','age','climate_region'))
+
 # DYNAMIC MAX MIN
 
 # figure out the ratio of max/min deaths over time by sex, age, year
@@ -58,11 +93,11 @@ lin.reg.grad.region$end.value <- with(lin.reg.grad.region,`(Intercept)`+year.cen
 # obtain significance of slopes
 lin.reg.sig.region <- ddply(dat.max.min, .(sex,age,climate_region), function(z)coef(summary(lm(percent.change ~ year.centre, data=z))))
 lin.reg.sig.region <- lin.reg.sig.region[!c(TRUE,FALSE),]
-lin.reg.sig.region$sig.test.10 <- ifelse(lin.reg.sig.region[,6]<0.10,1,0)
-lin.reg.sig.region$sig.test.5 <- ifelse(lin.reg.sig.region[,6]<0.05,1,0)
+lin.reg.sig.region$sig.test.10 <- ifelse(lin.reg.sig.region[,7]<0.10,1,0)
+lin.reg.sig.region$sig.test.5 <- ifelse(lin.reg.sig.region[,7]<0.05,1,0)
 
 # merge with data about gradients
-lin.reg.grad <- merge(lin.reg.grad.region,lin.reg.sig.region,by=c('sex','age','climate_region'))
+lin.reg.grad.region <- merge(lin.reg.grad.region,lin.reg.sig.region,by=c('sex','age','climate_region'))
 
 ###############################################################
 # DIRECTORY CREATION
@@ -76,4 +111,12 @@ ifelse(!dir.exists(file.loc), dir.create(file.loc, recursive=TRUE), FALSE)
 # EXPORT DATA
 ###############################################################
 
-saveRDS(lin.reg.grad,paste0(file.loc,'seasonality_index_',dname,'_',metric,'_',year.start.2,'_',year.end.2))
+# fixed max/min
+saveRDS(lin.reg.grad,paste0(file.loc,'seasonality_index_com_fixed_',dname,'_',metric,'_',year.start.2,'_',year.end.2))
+
+# dynamic max/min
+saveRDS(lin.reg.grad.region,paste0(file.loc,'seasonality_index_',dname,'_',metric,'_',year.start.2,'_',year.end.2))
+
+
+
+
