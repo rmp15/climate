c2bcdb5543a51e5b34ebca2e0ae6b6483afd07ba
Robbie Parks <r.parks15@imperial.ac.uk>
2017-01-23 17:07:51 +0000

    added stuff

--- countries/USA/prog/00_bash/metric_create.sh
+++ countries/USA/prog/00_bash/metric_create.sh
@@ -36,7 +36,7 @@ echo "creating temperature metric for counties for $year";
 
 # creates metrics from the temperature values processed
 # NEEDS TO OUTPUT VARIABLES TO A LOCATION WHERE THEY CAN BE READ BY THE PLOTTING FUNCTION
-#Rscript ~/git/climate/countries/USA/prog/05_metrics_development/metrics_development.R $year $dname 
+Rscript ~/git/climate/countries/USA/prog/05_metrics_development/metrics_development.R $year $dname 
 
 # SEE ABOVE ABOUT LOCATION OF VARIABLES FOR PLOTTING FUNCTION
 declare -a metrics=("mean" "days_below_10" "days_above_30" "sd" "days_changing_by_5" "days_increasing_by_5" "days_decreasing_by_5" "number_of_min_3_day_above_25_upwaves" "number_of_min_3_day_below_5_downwaves")
@@ -55,7 +55,7 @@ for metric in "${metrics[@]}"; do
 
 :
 # bind together data for each variable
-#Rscript ~/git/climate/countries/USA/prog/08_bind_results/bind_results.R ${years[0]} ${years[-1]} $dname $metric
+Rscript ~/git/climate/countries/USA/prog/08_bind_results/bind_results.R ${years[0]} ${years[-1]} $dname $metric
 
 done;
 
@@ -63,7 +63,7 @@ for metric in "${metrics[@]}"; do
 
 :
 # create climate region values
-#Rscript ~/git/climate/countries/USA/prog/05_metrics_development/metrics_development_climate_regions.R 1982 2015 1982 2013 $dname $metric
+Rscript ~/git/climate/countries/USA/prog/05_metrics_development/metrics_development_climate_regions.R 1982 2015 1982 2013 $dname $metric
 
 done;
 
@@ -71,7 +71,7 @@ for metric in "${metrics[@]}"; do
 
 :
 # create seasonality index climate values
-#Rscript ~/git/climate/countries/USA/prog/10_seasonality_index/seasonality_index_climate_regions.R 1982 2013 1982 2013 $dname $metric
+Rscript ~/git/climate/countries/USA/prog/10_seasonality_index/seasonality_index_climate_regions.R 1982 2013 1982 2013 $dname $metric
 
 done;
 
