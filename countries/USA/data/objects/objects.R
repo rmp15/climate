d04bf2d552176d710cd3916593d64245fd01a39e
rmp15 <r.parks15@imperial.ac.uk>
2017-04-05 14:58:48 +0100

    added correlatons

---
+++ countries/USA/data/objects/objects.R
@@ -0,0 +1,2 @@
+# climate metrics currently
+metrics <- c("mean","meanc","meanc2","days_below_10","days_above_30","sd","days_changing_by_5","days_increasing_by_5" ,"days_decreasing_by_5","number_of_min_3_day_above_99_upwaves","number_of_min_3_day_below_99_downwaves")
