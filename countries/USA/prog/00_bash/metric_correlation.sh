7b2f62bdc71f11665284f93555be9f1a72415eea
rmp15 <r.parks15@imperial.ac.uk>
2017-05-17 16:25:41 +0100

    added stuff

--- countries/USA/prog/00_bash/metric_correlation.sh
+++ countries/USA/prog/00_bash/metric_correlation.sh
@@ -5,10 +5,10 @@
 
 clear
 
-declare -i start=1980
-declare -i end=2013
+declare -i start=1979
+declare -i end=2015
 declare dname="t2m"
-declare metrics -a = ("meanc" "days_changing_by_5" "number_of_min_3_day_above_nonnormal_90_upwaves" "number_of_min_3_day_above_+5_jumpupwaves")
+declare -a metrics=("meanc" "days_changing_by_5" "number_of_min_3_day_above_nonnormal_90_upwaves" "number_of_min_3_day_above_+5_jumpupwaves" "number_of_days_above_nonnormal_90")
 
 for metric1 in "${metrics[@]}"; do
 for metric2 in "${metrics[@]}"; do
