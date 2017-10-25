f61593e9f1d28274e95485690893a7e4c3db7790
rmp15 <r.parks15@imperial.ac.uk>
2017-10-25 09:53:43 +0100

    added stuff

--- countries/USA/prog/00_bash/metric_correlation.sh
+++ countries/USA/prog/00_bash/metric_correlation.sh
@@ -8,7 +8,8 @@ clear
 declare -i start=1979
 declare -i end=2015
 declare dname="t2m"
-declare -a metrics=("meanc3" "10percc3" "90percc3")
+#declare -a metrics=('mean' 'meanc3' '10percc3' '90percc3' 'number_of_min_3_day_above_nonnormal_90_upwaves_2' 'number_of_min_3_day_above_+5_jumpupwaves_2' 'number_of_min_3_day_below_nonnormal_90_downwaves_2' 'number_of_min_3_day_below_+5_jumpdownwaves_2')
+declare -a metrics=('number_of_min_3_day_above_nonnormal_90_upwaves_2' 'number_of_min_3_day_above_+5_jumpupwaves_2')
 
 for metric1 in "${metrics[@]}"; do
 for metric2 in "${metrics[@]}"; do
