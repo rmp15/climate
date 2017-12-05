dd427642412957bd5fcef9b4ddcdbd056e2e7300
rmp15 <r.parks15@imperial.ac.uk>
2017-12-05 15:14:12 +0000

    Subnational population in progress

--- countries/USA/prog/00_bash/netcdf_download.sh
+++ countries/USA/prog/00_bash/netcdf_download.sh
@@ -24,14 +24,14 @@ declare -a dnames=("t2m" "d2m")
 
 (
 
-python ~/git/climate/countries/USA/prog/01_extract_netcdf/downloading_netcdf_files_usa_years_sep.py $start $end 't2m';
-python ~/git/climate/countries/USA/prog/01_extract_netcdf/downloading_netcdf_files_usa_years_sep.py $start $end 'd2m';
+#python ~/git/climate/countries/USA/prog/01_extract_netcdf/downloading_netcdf_files_usa_years_sep.py $start $end 't2m';
+#python ~/git/climate/countries/USA/prog/01_extract_netcdf/downloading_netcdf_files_usa_years_sep.py $start $end 'd2m';
 
-) &
+#) &
 
 #(
 
-#python ~/git/climate/countries/USA/prog/01_extract_netcdf/downloading_netcdf_files_usa_years_together.py $start $end 't2m';
-#python ~/git/climate/countries/USA/prog/01_extract_netcdf/downloading_netcdf_files_usa_years_together.py $start $end 'd2m';
+python ~/git/climate/countries/USA/prog/01_extract_netcdf/downloading_netcdf_files_usa_years_together.py $start $end 't2m';
+python ~/git/climate/countries/USA/prog/01_extract_netcdf/downloading_netcdf_files_usa_years_together.py $start $end 'd2m';
 
-#) &
+) &
