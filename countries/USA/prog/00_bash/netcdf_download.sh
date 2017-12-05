94e585525bcb6d5c5a303830e39f585f38944037
rmp15 <r.parks15@imperial.ac.uk>
2017-12-05 10:52:51 +0000

    Add python script for downloading ERA-Interim files

--- countries/USA/prog/00_bash/netcdf_download.sh
+++ countries/USA/prog/00_bash/netcdf_download.sh
@@ -11,10 +11,8 @@ declare -a dname=("t2m")
 
 for dname in "${dnames[@]}"; do
 
-
-
 echo "processing for $dname years $start - $end"
 
-python ~/git/climate/countries/USA/prog/01_extract_netcdf/downloading_netcdf_files_years_sep.py $start $end dname
+python ~/git/climate/countries/USA/prog/01_extract_netcdf/downloading_netcdf_files_years_sep.py $start $end $dname
 
 done;
