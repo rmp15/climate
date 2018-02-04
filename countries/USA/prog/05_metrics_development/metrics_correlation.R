c368dceef7591c2b10507824afe11d599e3a6669
rmp15 <r.parks15@imperial.ac.uk>
2018-02-04 14:23:08 +0000

    Add python script for downloading ERA-Interim files

--- countries/USA/prog/05_metrics_development/metrics_correlation.R
+++ countries/USA/prog/05_metrics_development/metrics_correlation.R
@@ -16,24 +16,25 @@ args <- commandArgs(trailingOnly=TRUE)
 year.start <- as.numeric(args[1])
 year.end <- as.numeric(args[2])
 dname.1 <- as.character(args[3])
-metric.1 <- as.character(args[4])
-metric.2 <- as.character(args[5])
+dname.2 <- as.character(args[4])
+metric.1 <- as.character(args[5])
+metric.2 <- as.character(args[6])
 
 year.start = 1979 ; year.end = 2015 ;
-dname = 't2m'
+dname.1 = 'tapp' ; dname.2 = 't2m'
 metric.1 = 'meanc3'
 metric.2 = 'meanc3'
 
 # create output directory
-dir = paste0("../../output/metrics_correlation/",dname,"/",metric.1,"/")
+dir = paste0("../../output/metrics_correlation/",dname.1,"/",metric.1,"/")
 ifelse(!dir.exists(dir), dir.create(dir,recursive=TRUE), FALSE)
 
 # load climate metrics
 source('../../data/objects/objects.R')
 
 # load metric of interest
-dat.1 <- readRDS(paste0('../../output/metrics_development/',dname,'/',metric.1,'_',dname,'/state_weighted_summary_',metric.1,'_',dname,'_',year.start,'_',year.end,'.rds'))
-dat.2 <- readRDS(paste0('../../output/metrics_development/',dname,'/',metric.2,'_',dname,'/state_weighted_summary_',metric.2,'_',dname,'_',year.start,'_',year.end,'.rds'))
+dat.1 <- readRDS(paste0('../../output/metrics_development/',dname.1,'/',metric.1,'_',dname.1,'/state_weighted_summary_',metric.1,'_',dname.1,'_',year.start,'_',year.end,'.rds'))
+dat.2 <- readRDS(paste0('../../output/metrics_development/',dname.2,'/',metric.2,'_',dname.2,'/state_weighted_summary_',metric.2,'_',dname.2,'_',year.start,'_',year.end,'.rds'))
 
 # subset for only one age-sex group
 dat.1 <- subset(dat.1,age==85&sex==2)
@@ -49,9 +50,10 @@ lm = lm(metric.2~metric.1,data=dat.plot)
 r.squared = round(summary(lm)$r.squared,2)
 
 # plot metric 1 against metric 2 for all states and all months
-pdf(paste0(dir,dname,'_',metric.2,'_against_',metric.1,'_',year.start,'_',year.end,'.pdf'),paper='a4r',height=0,width=0)
+pdf(paste0(dir,dname.1,'_',metric.2,'_against_',dname.2,'_',metric.2,'_',year.start,'_',year.end,'.pdf'),paper='a4r',height=0,width=0)
 print(ggplot() + geom_point(data=dat.plot,aes(x=metric.1,y=metric.2)) +
-xlab(metric.1) + ylab(metric.2) +
+geom_abline(a=1,b=0,lintype='dotted') +
+xlab(paste0(dname.1,'_',metric.1)) + ylab(paste0(dname.2,'_',metric.2)) +
 ggtitle(paste0(metric.2,' against ',metric.1,' with R^2=',r.squared)))
 dev.off()
 
@@ -59,14 +61,15 @@ dev.off()
 state.lookup = read.csv('~/git/mortality/USA/state/data/fips_lookup/name_fips_lookup.csv')
 
 # plot metric 1 against metric 2 for an individual state and facetted by month
-pdf(paste0(dir,dname,'_',metric.2,'_against_',metric.1,'_by_state_and_month_',year.start,'_',year.end,'.pdf'), paper='a4r',height=0,width=0)
+pdf(paste0(dir,dname.1,'_',metric.2,'_against_',dname.2,'_',metric.2,'_against_',metric.1,'_by_state_and_month_',year.start,'_',year.end,'.pdf'), paper='a4r',height=0,width=0)
 
 for (i in (unique(dat.plot$state.fips))) {
 
 state.name = state.lookup[state.lookup$fips==as.numeric(i),][1,1]
 
 print(ggplot(data=subset(dat.plot,state.fips==i),aes(x=metric.1,y=metric.2)) + geom_point() +
-xlab(metric.1) + ylab(metric.2) +
+xlab(paste0(dname.1,'_',metric.1)) + ylab(paste0(dname.2,'_',metric.2)) +
+geom_abline(a=1,b=0,lintype='dotted') +
 stat_smooth(method='loess') +
 ggtitle(paste0(state.name,' ',metric.2,' against ',metric.1)) +
 facet_wrap(~month))
