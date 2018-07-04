3c6ad04bf14fb45dadb19d8cdb8e2418d06e08f9
rmp15 <rparks15@imperial.ac.uk>
2018-07-04 12:13:57 +0100

    added plot of mean against range

--- countries/USA/prog/10_seasonality_index/seasonality_index_against_mean.R
+++ countries/USA/prog/10_seasonality_index/seasonality_index_against_mean.R
@@ -33,100 +33,45 @@ dat <- readRDS(paste0(file.loc.input,'climate_region_values_',dname,'_',metric,'
 # rename climate variable to generic name
 names(dat)[grep(dname,names(dat))] <- 'variable'
 
-# fix temperature to absolute scale
-if(dname=='t2m'){
-    dat$variable <- dat$variable + 273.15
-}
-
 ###############################################################
 # DATA PROCESSING
 ###############################################################
 
-# 1. CLIMATE REGION
+# isolate to one age and sex
+dat = subset(dat,age==65&sex==2)
+dat$age=NULL ; dat$sex=NULL
 
-# STATIC MAX MIN DEFINED BY COM
+# process mean, range, min, max, per climate region and per year
+dat.summary = ddply(dat,.(climate_region,year),summarize,mean=mean(variable),min=min(variable),max=max(variable))
+dat.summary$range = with(dat.summary,max-min)
 
-# load com data to establish max min locations
-file.loc.nat.input <- paste0("~/git/mortality/USA/state/output/com/",year.start.2,'_',year.end.2,"/national/values/combined_results/")
-dat.COM <- readRDS(paste0(file.loc.nat.input,'com_inv_com_rates_national_values_method_2_entire_',cod,'_',year.start.2,'_',year.end.2))
-
-# round to get month required for merging
-dat.COM$COM.mean <- round(dat.COM$COM.mean)
-dat.COM$COM.mean <- ifelse(dat.COM$COM.mean==0,12,dat.COM$COM.mean)
-dat.COM$month <- dat.COM$COM.mean
-levels(dat.COM$sex) <- c(1,2)
-
-# figure out the ratio of max/min temperature over time with fixed max/min by sex, age, year
-dat.max.min.fixed <- merge(dat,dat.COM,by=c('age','sex','month'))
-dat.max.min.fixed <- ddply(dat.max.min.fixed,.(sex,age,year,climate_region), summarize,max=variable[type=='max'],month.max=month[type=='max'],min=variable[type=='min'],month.min=month[type=='min'])
-dat.max.min.fixed$ratio <- with(dat.max.min.fixed,abs(max-min)/abs(min))
-dat.max.min.fixed$percent.change <- round(100*(dat.max.min.fixed$ratio),1)
-dat.max.min.fixed$diff <- with(dat.max.min.fixed,max-min)
-
-# add time value that starts at 0
-dat.max.min.fixed$year.centre <- with(dat.max.min.fixed,year-year.start)
-
-# apply linear regression to each group by sex, age, month to find gradient
-#lin.reg.grad <- ddply(dat.max.min.fixed, .(sex,age,climate_region), function(z)coef(lm(percent.change ~ year.centre, data=z)))
-lin.reg.grad <- ddply(dat.max.min.fixed, .(sex,age,climate_region), function(z)coef(lm(diff ~ year.centre, data=z)))
-lin.reg.grad$end.value <- with(lin.reg.grad,`(Intercept)`+year.centre*(num.years-1))
-lin.reg.grad$start.value <- lin.reg.grad$`(Intercept)`
-
-# obtain significance of slopes
-#lin.reg.sig <- ddply(dat.max.min.fixed, .(sex,age,climate_region), function(z)coef(summary(lm(percent.change ~ year.centre, data=z))))
-lin.reg.sig <- ddply(dat.max.min.fixed, .(sex,age,climate_region), function(z)coef(summary(lm(diff ~ year.centre, data=z))))
-lin.reg.sig <- lin.reg.sig[!c(TRUE,FALSE),]
-lin.reg.sig$sig.test.10 <- ifelse(lin.reg.sig[,7]<0.10,1,0)
-lin.reg.sig$sig.test.5 <- ifelse(lin.reg.sig[,7]<0.05,1,0)
-
-# merge with data about gradients
-lin.reg.grad <- merge(lin.reg.grad,lin.reg.sig,by=c('sex','age','climate_region'))
-#
-# # DYNAMIC MAX MIN
-#
-# # figure out the ratio of max/min deaths over time by sex, age, year
-# dat.max.min <-  ddply(dat, .(sex,age,year,climate_region), summarize, max=max(variable),month.max=month[variable==max(variable)],min=min(variable),month.min=month[variable==min(variable)])
-# dat.max.min$ratio <- with(dat.max.min,abs(max-min)/abs(min))
-# dat.max.min$percent.change <- round(100*(dat.max.min$ratio),1)
-# dat.max.min$diff <- with(dat.max.min,max-min)
-#
-# # add time value that starts at 0
-# dat.max.min$year.centre <- with(dat.max.min,year-year.start)
-#
-# # apply linear regression to each group by sex, age, month to find gradient
-# #lin.reg.grad.region <- ddply(dat.max.min, .(sex,age,climate_region), function(z)coef(lm(percent.change ~ year.centre, data=z)))
-# lin.reg.grad.region <- ddply(dat.max.min, .(sex,age,climate_region), function(z)coef(lm(diff ~ year.centre, data=z)))
-# lin.reg.grad.region$start.value <- lin.reg.grad.region$`(Intercept)`
-# lin.reg.grad.region$end.value <- with(lin.reg.grad.region,`(Intercept)`+year.centre*(num.years-1))
-#
-# # obtain significance of slopes
-# #lin.reg.sig.region <- ddply(dat.max.min, .(sex,age,climate_region), function(z)coef(summary(lm(percent.change ~ year.centre, data=z))))
-# lin.reg.sig.region <- ddply(dat.max.min, .(sex,age,climate_region), function(z)coef(summary(lm(diff ~ year.centre, data=z))))
-# lin.reg.sig.region <- lin.reg.sig.region[!c(TRUE,FALSE),]
-# lin.reg.sig.region$sig.test.10 <- ifelse(lin.reg.sig.region[,7]<0.10,1,0)
-# lin.reg.sig.region$sig.test.5 <- ifelse(lin.reg.sig.region[,7]<0.05,1,0)
-#
-# # merge with data about gradients
-# lin.reg.grad.region <- merge(lin.reg.grad.region,lin.reg.sig.region,by=c('sex','age','climate_region'))
+# find single values for each climate region by summarising yearly values
+dat.summary.summary = ddply(dat.summary,.(climate_region),summarize,mean=mean(mean),min=mean(min),max=mean(max))
+dat.summary.summary$range = with(dat.summary.summary,max-min)
 
 ###############################################################
 # DIRECTORY CREATION
 ###############################################################
 
 # create directories for output
-file.loc <- paste0('~/git/climate/countries/USA/output/seasonality_index_climate_region/',dname,'/',metric,'/')
+file.loc <- paste0('~/git/climate/countries/USA/output/seasonality_index_against_mean/',dname,'/',metric,'/')
 ifelse(!dir.exists(file.loc), dir.create(file.loc, recursive=TRUE), FALSE)
 
 ###############################################################
-# EXPORT DATA
+# PLOT DATA
 ###############################################################
 
 # fixed max/min
-saveRDS(lin.reg.grad,paste0(file.loc,'seasonality_index_com_fixed_',dname,'_',metric,'_',cod,'_',year.start.2,'_',year.end.2))
-
-# dynamic max/min
-#saveRDS(lin.reg.grad.region,paste0(file.loc,'seasonality_index_',dname,'_',metric,'_',year.start.2,'_',year.end.2))
-
-
-
-
+pdf(paste0(file.loc,'comparison_of_mean_against_range_',year.start,'_',year.end,'.pdf'),height=0,width=0,paper='a4r')
+ggplot() +
+    geom_point(data=dat.summary,aes(x=mean,y=range,color=climate_region)) +
+    geom_point(data=dat.summary.summary,aes(x=mean,y=range),fill='black',size=7) +
+    geom_point(data=dat.summary.summary,aes(x=mean,y=range,color=climate_region),fill='black',size=5) +
+    xlab('Mean temperature') + ylab('Temperature range') +
+    theme_bw() + theme(panel.grid.major = element_blank(),axis.text.x = element_text(angle=0),
+    plot.title = element_text(hjust = 0.5),panel.background = element_blank(),
+    panel.grid.minor = element_blank(), axis.line = element_line(colour = "black"),
+    panel.border = element_rect(colour = "black"),strip.background = element_blank(),
+    legend.position = 'bottom',legend.justification='center',
+    legend.background = element_rect(fill="gray90", size=.5, linetype="dotted"))
+dev.off()
