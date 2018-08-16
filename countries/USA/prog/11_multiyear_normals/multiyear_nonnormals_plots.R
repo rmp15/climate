5d3059775a8fe948e27cac9aed1721a96b5aa75e
rmp15 <r.parks15@imperial.ac.uk>
2018-08-16 17:22:37 +0100

    Add python script for downloading ERA-Interim files

--- countries/USA/prog/11_multiyear_normals/multiyear_nonnormals_plots.R
+++ countries/USA/prog/11_multiyear_normals/multiyear_nonnormals_plots.R
@@ -4,6 +4,12 @@ library(ggplot2)
 library(foreign)
 library(plyr)
 library(lubridate)
+library(maptools)
+library(mapproj)
+library(rgeos)
+library(rgdal)
+library(RColorBrewer)
+library(scales)
 
 # arguments from Rscript
 args <- commandArgs(trailingOnly=TRUE)
@@ -12,7 +18,9 @@ year.end <- as.numeric(args[2])
 dname <- as.character(args[3])
 metric <- as.character(args[4])
 
-cat('processing for years',year.start,year.end)
+year.start=1980 ; year.end=2009 ; dname='t2m' ; metric='mean'
+
+cat('plotting for years',year.start,year.end)
 
 directory = paste0("../../output/multiyear_normals_plots/",dname,"/",metric)
 ifelse(!dir.exists(directory), dir.create(directory, recursive=TRUE), FALSE)
@@ -73,4 +81,85 @@ ggplot(data=dat) +
     panel.border = element_rect(colour = "black"),strip.background = element_blank(),
     legend.position = 'bottom',legend.justification='center',
     legend.background = element_rect(fill="gray90", size=.5, linetype="dotted"))
+dev.off()
+
+###############################################################
+# PREPARING MAP
+###############################################################
+
+# for theme_map
+#devtools::source_gist("33baa3a79c5cfef0f6df")
+theme_map <- function(base_size=9, base_family=""){
+    require(grid)
+    theme_bw(base_size=base_size,base_family=base_family) %+replace%
+    theme(axis.line=element_blank(),
+    axis.text=element_blank(),
+    axis.ticks=element_blank(),
+    axis.title=element_blank(),
+    panel.background=element_blank(),
+    panel.border=element_blank(),
+    panel.grid=element_blank(),
+    panel.margin=unit(0,"lines"),
+    plot.background=element_blank(),
+    legend.justification = c(0,0),
+    legend.position = c(0,0)
+    )
+}
+
+# load shapefile
+us <- readOGR(dsn="../../../../../mortality/USA/state/data/shapefiles",layer="states")
+
+# convert shapefile to Albers equal area
+us_aea <- spTransform(us, CRS("+proj=laea +lat_0=45 +lon_0=-100 +x_0=0 +y_0=0 +a=6370997 +b=6370997 +units=m +no_defs"))
+us_aea@data$id <- rownames(us_aea@data)
+
+# extract, then rotate, shrink & move alaska (and reset projection)
+alaska <- us_aea[us_aea$STATE_FIPS=="02",]
+alaska <- elide(alaska,rotate=-50)
+alaska <- elide(alaska,scale=max(apply(bbox(alaska), 1, diff)) / 2.3)
+alaska <- elide(alaska, shift=c(-2100000, -2500000))
+proj4string(alaska) <- proj4string(us_aea)
+
+# extract, then rotate & shift hawaii
+hawaii <- us_aea[us_aea$STATE_FIPS=="15",]
+hawaii <- elide(hawaii, rotate=-35)
+hawaii <- elide(hawaii, shift=c(5400000, -1400000))
+proj4string(hawaii) <- proj4string(us_aea)
+
+# remove Alaska and Hawaii (don't put back in now)
+us_aea <- us_aea[!us_aea$STATE_FIPS %in% c("02", "15"),]
+# us_aea <- rbind(us_aea, alaska, hawaii)
+
+# fortify to prepare for ggplot
+map <- fortify(us_aea)
+
+# extract data from shapefile
+shapefile.data <- us_aea@data
+
+# reinsert shapefile.data with climate regions back into shapefile if changed
+us_aea@data <- shapefile.data
+
+# merge selected data to map dataframe for colouring of ggplot
+USA.df <- merge(map, shapefile.data, by='id')
+USA.df$STATE_FIPS <- as.integer(as.character(USA.df$STATE_FIPS))
+
+dat$state.fips = as.integer(dat$state.fips)
+
+USA.df.month = merge(USA.df,dat,by.y='state.fips',by.x='STATE_FIPS')
+USA.df.month <- with(USA.df.month, USA.df.month[order(month,DRAWSEQ,order),])
+
+# good colours
+f <- function(pal) brewer.pal(brewer.pal.info[pal, "maxcolors"], pal)
+mycols <- c(f("Dark2"), f("Set1")[1:8], f("Set2"), f("Set3"),"#89C5DA", "#DA5724", "#74D944", "#CE50CA", "#3F4921", "#C0717C", "#CBD588", "#5F7FC7", "#673770", "#D3D93E", "#38333E", "#508578", "#D7C1B1", "#689030", "#AD6F3B", "#CD9BCD", "#D14285", "#6DDE88", "#652926", "#7FDCC0", "#C84248", "#8569D5", "#5E738F", "#D1A33D", "#8A7C64", "#599861" )
+#plot(1:length(mycols),col=mycols[1:length(mycols)],cex=4,pch=20); abline(v=c(10,20,30,40,50,60))
+
+pdf(paste0(directory,'/longterm_nonnormals_map.pdf'),height=0,width=0,paper='a4r')
+print(ggplot(data=subset(USA.df.month),aes(x=long,y=lat,group=group)) +
+geom_polygon(aes(fill=variable),color='black',size=0.01) +
+scale_fill_gradient2(high="dark red",mid="light blue",low="dark blue",guide = 'colorbar',guide_legend(title="Temperature (°C)")) +
+facet_wrap(~month.short) +
+xlab('') +
+ylab('') +
+theme_map() +
+theme(text = element_text(size = 15),legend.position = 'bottom',legend.justification=c(0.5,0.5),strip.background = element_blank(),legend.background = element_rect(fill = "grey95")))
 dev.off()
