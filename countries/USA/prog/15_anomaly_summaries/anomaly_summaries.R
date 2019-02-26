4e12635e0c9fa11d82246668a177386ab1e77f86
rmp15 <r.parks15@imperial.ac.uk>
2019-02-26 11:10:15 +0000

    Add python script for downloading ERA-Interim files

--- countries/USA/prog/15_anomaly_summaries/anomaly_summaries.R
+++ countries/USA/prog/15_anomaly_summaries/anomaly_summaries.R
@@ -141,6 +141,46 @@ ggplot(data=dat.anom.abs)+
     legend.background = element_rect(fill="gray90", size=.5, linetype="dotted"))
 dev.off()
 
+pdf(paste0(output.dir,'heatmap_square_plot_uneven_scale_alt4_',year.start,'_',year.end,'_',dname,'_anom_abs.pdf'),paper='a4',height=0,width=0)
+ggplot(data=dat.anom.abs)+
+    geom_tile(aes(x=month,y=full_name,fill=anom.abs)) +
+    coord_equal() +
+    scale_fill_gradientn(colours = c("#ffffe5","#fff7bc","#fee391","#fec44f","#fe9929","#ec7014","#662506",'#dd3497','#ae017e','#7a0177','#49006a'),
+    values = c(0,0.2,0.25,0.3,0.35,0.4,0.5,0.6,0.65,0.7,0.8,1),
+    breaks=seq(0,3.5,1), limits = c(0,3.5),
+    guide = guide_legend(nrow = 1,title = paste0("Average absolute anomaly"))) +
+    guides(fill = guide_colorbar(barwidth = 5, barheight = 1,title = expression("Average size of anomaly " (degree*C)))) +
+    scale_x_continuous(breaks=c(seq(1,12,by=1)),labels=month.short) +
+    # coord_flip() +
+    xlab("Month") + ylab('') +
+    theme_bw() + theme(panel.grid.major = element_blank(),axis.text.x = element_text(angle=90),
+    plot.title = element_text(hjust = 0.5),panel.background = element_blank(),
+    panel.grid.minor = element_blank(), axis.line = element_line(colour = "black"),
+    panel.border = element_rect(colour = "black"),strip.background = element_blank(),
+    legend.position = 'bottom',legend.justification='center',
+    legend.background = element_rect(fill="gray90", size=.5, linetype="dotted"))
+dev.off()
+
+pdf(paste0(output.dir,'heatmap_square_plot_uneven_scale_alt5_',year.start,'_',year.end,'_',dname,'_anom_abs.pdf'),paper='a4',height=0,width=0)
+ggplot(data=dat.anom.abs)+
+    geom_tile(aes(x=month,y=full_name,fill=anom.abs)) +
+    coord_equal() +
+    scale_fill_gradientn(colours = c("#ffffe5","#fff7bc","#fee391","#fec44f","#fe9929","#ec7014","#662506",'#1d91c0','#225ea8','#253494','#081d58'),
+    values = c(0,0.2,0.25,0.3,0.35,0.4,0.5,0.6,0.65,0.7,0.8,1),
+    breaks=seq(0,3.5,1), limits = c(0,3.5),
+    guide = guide_legend(nrow = 1,title = paste0("Average absolute anomaly"))) +
+    guides(fill = guide_colorbar(barwidth = 5, barheight = 1,title = expression("Average size of anomaly " (degree*C)))) +
+    scale_x_continuous(breaks=c(seq(1,12,by=1)),labels=month.short) +
+    # coord_flip() +
+    xlab("Month") + ylab('') +
+    theme_bw() + theme(panel.grid.major = element_blank(),axis.text.x = element_text(angle=90),
+    plot.title = element_text(hjust = 0.5),panel.background = element_blank(),
+    panel.grid.minor = element_blank(), axis.line = element_line(colour = "black"),
+    panel.border = element_rect(colour = "black"),strip.background = element_blank(),
+    legend.position = 'bottom',legend.justification='center',
+    legend.background = element_rect(fill="gray90", size=.5, linetype="dotted"))
+dev.off()
+
 # LEGACY CODE BELOW
 
 # # dots
