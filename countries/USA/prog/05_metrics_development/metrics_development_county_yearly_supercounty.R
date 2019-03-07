e1d9ff97bc2bc02807c879d75f944d2c16c66e9e
rmp15 <rparks15@imperial.ac.uk>
2019-03-07 15:08:38 +0000

    added stuff

--- countries/USA/prog/05_metrics_development/metrics_development_county_yearly_supercounty.R
+++ countries/USA/prog/05_metrics_development/metrics_development_county_yearly_supercounty.R
@@ -5,36 +5,62 @@ library(plyr)
 
 # arguments from Rscript
 args <- commandArgs(trailingOnly=TRUE)
-year <- as.numeric(args[1])
-dname <- as.character(args[2])
-var <- as.character(args[3])
+year.start <- as.numeric(args[1])
+year.end <- as.numeric(args[2])
+dname <- as.character(args[3])
+var <- as.character(args[4])
 
-print(year)
+# years of study
+years = c(year.start:year.end)
 
-# load county summary by day (for single year)
+# load county summary
 dir.input = paste0("../../output/metrics_development_county/",dname,'/',var,'_',dname,'/')
-dat.county <- readRDS(paste0(dir.input,'county_summary_',var,'_',dname,'_',year,'.rds'))
+
+# cycle through years adding all data together
+dat.county = data.frame()
+for(year in years) {
+    print(year)
+    dat.county.current <- readRDS(paste0(dir.input,'county_summary_',var,'_',dname,'_',year,'.rds'))
+    dat.county = rbind(dat.county,dat.county.current)
+}
 print('weather data loaded')
 
 # load population data
 load('~/git/pollution/countries/USA/data/population/raw/nchs_raw_annotated_withag_1990_to_2016')
-pop_nchs_allage <- as.data.frame(summarise(group_by(subset(dat_nchs,sex==id_sex),year,fips,sex),popsum=sum(popsum)))
+# pop_nchs_allage <- as.data.frame(summarise(group_by(subset(dat_nchs,sex==id_sex),year,fips,sex),popsum=sum(popsum)))
+id_sex=1
+dat_nchs = as.data.frame(dat_nchs)
+pop_nchs_allage = ddply(subset(dat_nchs,sex==id_sex),.(year,fips,sex),summarize,popsum=sum(popsum))
 popsum_nchs <- subset(pop_nchs_allage,sex==id_sex)
-ap_pop_nchs <- left_join(dat.county,popsum_nchs)
+# ap_pop_nchs <- left_join(dat.county,popsum_nchs)
+
+# fix county codes for transient fips codes
+# 46 113 to 46 102
+popsum_nchs$fips[popsum_nchs$fips=='46113'] <- '46102'
+
+# merge county yearly data with
+ap_pop_nchs <- merge(dat.county,popsum_nchs,by.x=c('year','state.county.fips'),by.y=c('year','fips'),all.x=TRUE)
+ap_pop_nchs$fips = ap_pop_nchs$state.county.fips ; ap_pop_nchs$state.county.fips = NULL
 
 # account for missing data due to merging/splitting counties
 ap_pop_nchs[ap_pop_nchs$fips == '08014' & ap_pop_nchs$year <= 1999,'popsum'] <- popsum_nchs[popsum_nchs$fips == '08014' & popsum_nchs$year == 2000,'popsum']
 ap_pop_nchs[ap_pop_nchs$fips == '46113' & ap_pop_nchs$year %in% c(2010,2011,2012,2013,2014,2015),'popsum'] <- popsum_nchs[popsum_nchs$fips == '46102' & popsum_nchs$year %in% c(2010,2011,2012,2013,2014,2015),'popsum']
 ap_pop_nchs[ap_pop_nchs$fips == '51515' & ap_pop_nchs$year %in% c(2010,2011,2012,2013,2014,2015),'popsum'] <- popsum_nchs[popsum_nchs$fips == '51515' & popsum_nchs$year %in% c(2009),'popsum']
 
+# remove sex as unnecessary
+ap_pop_nchs$sex = NULL
+
+# examine NAs in above
+ap_popnchs_na = ap_pop_nchs[rowSums(is.na(ap_pop_nchs))>0,]
+
 # load myserious functions
-source('../../functions/combine_sc.R')
-source('../../functions/combine_rc.R')
+source('~/git/pollution/countries/USA/prog/04_supercounty_functions/combine_sc.R')
+source('~/git/pollution/countries/USA/prog/04_supercounty_functions/combine_mc.R')
 
 # merging counties with population-weighted pollution
-ap_pop_nchs_sctag <- combine_sc(ap_pop_nchs,scloc.df.sc) # WHAT IS combine_sc ?
+ap_pop_nchs_sctag <- combine_sc(ap_pop_nchs,scloc.df.sc)
 ap_pop_nchs_sctag$appop <- ap_pop_nchs_sctag$pred.wght*ap_pop_nchs_sctag$popsum
 ap_pop_nchs_sc <- data.frame(summarise(group_by(ap_pop_nchs_sctag,fips,year),apsc.wght=sum(appop)/sum(popsum),popsum=sum(popsum)))
-ap_pop_nchs_mctag <- combine_mc(ap_pop_nchs_sc,scloc.df) # WHAT IS combine_mc ?
+ap_pop_nchs_mctag <- combine_mc(ap_pop_nchs_sc,scloc.df)
 ap_pop_nchs_mctag$appop <- ap_pop_nchs_mctag$apsc.wght*ap_pop_nchs_mctag$popsum
 ap_pop_nchs_mc <- data.frame(summarise(group_by(ap_pop_nchs_mctag,fips,year),apmc.wght=sum(appop)/sum(popsum)))
