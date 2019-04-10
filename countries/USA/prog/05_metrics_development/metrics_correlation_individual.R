06c16140343f9395fcbd3188aa940e98d61bf168
rmp15 <rparks15@imperial.ac.uk>
2019-04-10 12:10:14 +0100

    added stuff

--- countries/USA/prog/05_metrics_development/metrics_correlation_individual.R
+++ countries/USA/prog/05_metrics_development/metrics_correlation_individual.R
@@ -22,9 +22,9 @@ metric.1 <- as.character(args[5])
 metric.2 <- as.character(args[6])
 
 year.start = 1979 ; year.end = 2015 ;
-dname.1 = 't2m' ; dname.2 = 't2m'
-metric.1 = 'mean'
-metric.2 = 'sd'
+dname.1 = 't2m' ; dname.2 = 'tapp'
+metric.1 = 'meanc3'
+metric.2 = 'meanc3'
 
 # create output directory
 dir = paste0("../../output/metrics_correlation/",dname.1,"/",metric.1,"/")
@@ -36,6 +36,10 @@ source('../../data/objects/objects.R')
 # load metric of interest
 dat.1 <- readRDS(paste0('../../output/metrics_development/',dname.1,'/',metric.1,'_',dname.1,'/state_weighted_summary_',metric.1,'_',dname.1,'_',year.start,'_',year.end,'.rds'))
 dat.2 <- readRDS(paste0('../../output/metrics_development/',dname.2,'/',metric.2,'_',dname.2,'/state_weighted_summary_',metric.2,'_',dname.2,'_',year.start,'_',year.end,'.rds'))
+
+# only take from 1980-2016 which is my thesis analysis period
+dat.1 = subset(dat.1,year%in%c(1980:2016))
+dat.2 = subset(dat.2,year%in%c(1980:2016))
 
 # subset for only one age-sex group
 dat.1 <- subset(dat.1,age==85&sex==2) ; dat.1 = subset(dat.1,!(state.fips%in%c('02','15')))
@@ -49,6 +53,7 @@ dat.2 = with(dat.2,dat.2[order(sex,age,state.fips,month),])
 dat.plot = data.frame(month=dat.1$month,state.fips=dat.1$state.fips,metric.1=dat.1[,ncol(dat.1)],metric.2=dat.2[,ncol(dat.2)])
 lm = lm(metric.2~metric.1,data=dat.plot)
 r.squared = round(summary(lm)$r.squared,2)
+correlation = cor(dat.plot$metric.1,dat.plot$metric.2)
 
 # plot metric 1 against metric 2 for all states and all months
 pdf(paste0(dir,dname.1,'_',metric.2,'_against_',dname.2,'_',metric.2,'_',year.start,'_',year.end,'.pdf'),paper='a4r',height=0,width=0)
@@ -60,6 +65,10 @@ dev.off()
 
 # load state fips lookup
 state.lookup = read.csv('~/git/mortality/USA/state/data/fips_lookup/name_fips_lookup.csv')
+
+# attach state names for facetting
+dat.plot$fips = as.numeric(as.character(dat.plot$state.fips))
+dat.plot = merge(dat.plot,state.lookup,by=c('fips'),all.X=TRUE)
 
 # plot metric 1 against metric 2 for an individual state
 pdf(paste0(dir,dname.1,'_',metric.2,'_against_',dname.2,'_',metric.2,'_against_',metric.1,'_by_state_',year.start,'_',year.end,'.pdf'), paper='a4r',height=0,width=0)
@@ -78,16 +87,24 @@ ggtitle(paste0(state.name,' ',metric.2,' against ',metric.1)))
 
 dev.off()
 
-# plot metric 1 against metric 2 for an individual state and facetted by month
+# plot metric 1 against metric 2 facetted by state
 pdf(paste0(dir,dname.1,'_',metric.2,'_against_',dname.2,'_',metric.2,'_against_',metric.1,'_facet_by_state_',year.start,'_',year.end,'.pdf'), paper='a4r',height=0,width=0)
 
 print(ggplot(data=subset(dat.plot),aes(x=metric.1,y=metric.2)) + geom_point() +
-xlab(paste0(dname.1,'_',metric.1)) + ylab(paste0(dname.2,'_',metric.2)) +
-geom_abline(a=1,b=0,lintype='dotted') +
-stat_smooth(method='loess') +
-ggtitle(paste0(state.name,' ',metric.2,' against ',metric.1)) +
-facet_wrap(~state.fips))
-
+xlab(expression('Temperature anomaly (°C)')) + ylab(expression('Apparent temperature anomaly (°C)')) +
+coord_equal()+
+# xlab(paste0(dname.1,'_',metric.1)) + ylab(paste0(dname.2,'_',metric.2)) +
+geom_abline() +
+# stat_smooth(method='loess') +
+# ggtitle(paste0(state.name,' ',metric.2,' against ',metric.1)) +
+facet_wrap(~full_name) +
+theme_bw() + theme(panel.grid.major = element_blank(),axis.text.x = element_text(angle=0),
+plot.title = element_text(hjust = 0.5),panel.background = element_blank(),
+panel.grid.minor = element_blank(), axis.line = element_line(colour = "black"),
+panel.border = element_rect(colour = "black"),strip.background = element_blank(),
+legend.position = 'bottom',legend.justification='center',
+legend.background = element_rect(fill="gray90", size=.5, linetype="dotted"))
+)
 dev.off()
 
 
