f56f1ca9b9250c953d8c5facb2a903a77f05b44e
rmp15 <rparks15@imperial.ac.uk>
2019-05-13 13:00:24 +0100

    added stuff

--- countries/USA/prog/05_metrics_development/metrics_compare_county_state.R
+++ countries/USA/prog/05_metrics_development/metrics_compare_county_state.R
@@ -19,7 +19,7 @@ metric.1 <- as.character(args[4])
 metric.2 <- as.character(args[4])
 
 # create directory
-dir = paste0("../../output/metrics_compare_county_state/",dname,'/',metric.1,'/',metric.2)
+dir = paste0("../../output/metrics_compare_county_state/",dname,'/',metric.1,'/')
 ifelse(!dir.exists(dir), dir.create(dir, recursive=TRUE), FALSE)
 
 # load file with climate data
@@ -54,6 +54,7 @@ month.short <- c('Jan','Feb','Mar','Apr','May','Jun','Jul','Aug','Sep','Oct','No
 dat.summary$month.short <- mapvalues(dat.summary$month,from=sort(unique(dat.summary$month)),to=c(as.character(month.short)))
 dat.summary$month.short <- reorder(dat.summary$month.short,dat.summary$month)
 
+# save in case
 saveRDS(dat.summary,paste0('../../output/metrics_development/',dname,'/',metric.1,'_',dname,'/diff/summary_',year.start,'_',year.end))
 
 # load map and attach information to the map for plotting
@@ -126,45 +127,50 @@ dev.off()
 # PLOTTING
 ###############################################################
 
-pdf(paste0(dir,'/county_deviations',year.start,'_',year.end,'.pdf'),height=0,width=0,paper='a4r')
-
 # attach state names
 state.lookup <- read.csv('~/git/mortality/USA/state/data/fips_lookup/name_fips_lookup.csv')
 dat.summary$fips = as.numeric(dat.summary$state.fips)
 dat.summary = merge(dat.summary,state.lookup,by='fips',all.x=TRUE)
 
-ggplot(data=subset(dat.summary,age==65&sex==2&!(state.fips%in%c('02','15')))) +
-    geom_point(aes(x=state.fips,y=diff),alpha=0.5) +
-    geom_hline(yintercept=0,linetype='dotted') +
-    facet_wrap(~month)
+pdf(paste0(dir,'/county_deviations',year.start,'_',year.end,'.pdf'),height=0,width=0,paper='a4r')
+
+# ggplot(data=subset(dat.summary,age==65&sex==2&!(state.fips%in%c('02','15')))) +
+#     geom_point(aes(x=state.fips,y=diff),alpha=0.5) +
+#     geom_hline(yintercept=0,linetype='dotted') +
+#     facet_wrap(~month.short)
 
 ggplot(data=subset(dat.summary,age==65&sex==2&!(state.fips%in%c('02','15')))) +
-    geom_point(aes(x=code_name,y=diff,alpha=0.5,size=(pop.weighted))) +
+    geom_point(aes(x=code_name,y=diff,alpha=0.1,size=(pop.weighted))) +
     geom_hline(yintercept=0,linetype='dotted') +
     facet_wrap(~month.short) +
     ylim(c(-1,1)) +
-    xlab('State') + ylab('')
+    xlab('State') + ylab('Average county anomaly deviation from state anomaly (°C)') +
+    guides(size=FALSE,alpha=FALSE) +
     theme_bw() + theme( panel.grid.major = element_blank(),axis.text.x = element_text(size=5,angle=90),
     axis.ticks.x=element_blank(),
     panel.grid.minor = element_blank(), axis.line = element_line(colour = "black"),
     panel.border = element_rect(colour = "black"),strip.background = element_blank(),
     legend.position = 'bottom',legend.justification='center',
-    legend.background = element_rect(fill="gray90", size=.5, linetype="dotted"))
+    legend.background = element_rect(fill="white", size=.5, linetype="dotted"))
+dev.off()
+
+pdf(paste0(dir,'/county_deviations_',year.start,'_',year.end,'month_separate.pdf'),height=0,width=0,paper='a4r')
 
 for(i in sort(unique(dat.summary$month))){
     print(
     ggplot(data=subset(dat.summary,month==i&age==65&sex==2&!(state.fips%in%c('02','15')))) +
     geom_point(aes(x=code_name,y=diff,alpha=0.5,size=(pop.weighted))) +
     geom_hline(yintercept=0,linetype='dotted') +
-    facet_wrap(~month) +
+    facet_wrap(~month.short) +
     ylim(c(-1,1)) +
+    xlab('State') + ylab('Average county anomaly deviation from state anomaly (°C)') +
+    guides(size=FALSE,alpha=FALSE) +
     theme_bw() + theme( panel.grid.major = element_blank(),axis.text.x = element_text(size=5,angle=90),
     axis.ticks.x=element_blank(),
     panel.grid.minor = element_blank(), axis.line = element_line(colour = "black"),
     panel.border = element_rect(colour = "black"),strip.background = element_blank(),
     legend.position = 'bottom',legend.justification='center',
-    legend.background = element_rect(fill="gray90", size=.5, linetype="dotted"))
+    legend.background = element_rect(fill="white", size=.5, linetype="dotted"))
     )
 }
-
 dev.off()
