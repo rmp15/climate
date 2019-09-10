bc54888ac9e28cc8cd8a29188f07df9b3b32df22
rmp15 <r.parks15@imperial.ac.uk>
2019-09-10 17:45:10 -0400

    New plots

--- countries/USA/prog/00_bash/netcdf_download.sh
+++ countries/USA/prog/00_bash/netcdf_download.sh
@@ -41,5 +41,5 @@ declare -a dnames=("t2m" "d2m")
 
 # ERA5
 
-python3 ~/git/climate/countries/USA/prog/01_extract_netcdf/downloading_netcdf_files_worldwide_years_sep_era5_cdsapi.py $start $end 't2m';
+python ~/git/climate/countries/USA/prog/01_extract_netcdf/downloading_netcdf_files_worldwide_years_sep_era5_cdsapi.py $start $end 't2m';
 #python ~/git/climate/countries/USA/prog/01_extract_netcdf/downloading_netcdf_files_worldwide_years_sep.py $start $end 'd2m';
