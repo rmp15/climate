9c226441e275b83f527b16dd34ce1b6f6cae65f3
rmp15 <r.parks15@imperial.ac.uk>
2020-04-10 20:40:39 -0400

    covid 19 work

--- countries/UK/prog/01_extract_netcdf/extracting_netcdf_files_era5.R
+++ countries/UK/prog/01_extract_netcdf/extracting_netcdf_files_era5.R
@@ -28,19 +28,23 @@ num <- as.character(args[4])
 year <- as.character(args[5])
 space.res <- as.character(args[6])
 
+# create directory to place output files into
+dir.output = "../../output/grid_county_intersection_raster/"
+if(space.res=='lad'){dir.output=paste0(dir.output,'lads/',state,'/')}
+ifelse(!dir.exists(dir.output), dir.create(dir.output), FALSE)
+
 # load shapefile of entire United Kingdom originally from http://geoportal.statistics.gov.uk/datasets/ae90afc385c04d869bc8cf8890bd1bcd_1
-# uk.national <- readOGR(dsn="../../data/shapefiles/Local_Authority_Districts_December_2017_Full_Clipped_Boundaries_in_Great_Britain",layer="Local_Authority_Districts_December_2017_Full_Clipped_Boundaries_in_Great_Britain")
-uk.national <- readOGR(dsn="../../data/shapefiles/infuse_uk_2011_clipped",layer="infuse_uk_2011_clipped")
-
-# fix long to match raster
-uk.national$long = ifelse(uk.national$long<0, uk.national$long + 360, uk.national$long)
-
-print(paste0('running extracting_netcdf_files.R for ',date))
-
-dname <- 't2m' ; freq <- 'daily' ; num <- 'four' ; year <- '2010' ; space.res='lad'
+uk.national <- readOGR(dsn="../../data/shapefiles/Local_Authority_Districts_December_2017_Full_Clipped_Boundaries_in_Great_Britain",layer="Local_Authority_Districts_December_2017_Full_Clipped_Boundaries_in_Great_Britain")
 
 # get projection of shapefile
 original.proj = proj4string(uk.national)
+
+# fix long to match raster (is this right?)
+# uk.national$long = ifelse(uk.national$long<0, uk.national$long + 360, uk.national$long)
+
+dname <- 't2m' ; freq <- 'daily' ; num <- 'four' ; year <- '2010' ; space.res='lad'
+
+print(paste0('running extracting_netcdf_files.R for ',year))
 
 # function to perform analysis for entire country
 uk.analysis = function(uk.national,raster.input,output=0) {
@@ -61,12 +65,6 @@ uk.analysis = function(uk.national,raster.input,output=0) {
         # isolate zip to highlight
         uk.lad = uk.national[uk.national$lad17cd %in% lad,]
 
-        plot.map=fortify(uk.national)
-        proj4string(uk.national) = proj4string(us.national)
-        coordinates(uk.national) <- c('lon', 'lat')
-        print(ggplot() +
-        geom_polygon(data=subset(plot.map),aes(x=long,y=lat,group=group),color='white',size=0.01))
-
         current.value = extract(x=raster.input,weights = TRUE,normalizeWeights=TRUE,y=uk.lad,fun=mean,df=TRUE,na.rm=TRUE)
 
         to.add = data.frame(zip,value=current.value[1,2])
@@ -81,43 +79,46 @@ uk.analysis = function(uk.national,raster.input,output=0) {
 
 }
 
-
-
 # FINISH LATER
 # perform analysis across every day of selected year
-if(freq=='daily'){
-    # loop through each raster file for each day and summarise
-    dates <- seq(as.Date(paste0('0101',year),format="%d%m%Y"), as.Date(paste0('3112',year),format="%d%m%Y"), by=1)
+# loop through each raster file for each day and summarise
+dates <- seq(as.Date(paste0('0101',year),format="%d%m%Y"), as.Date(paste0('3112',year),format="%d%m%Y"), by=1)
 
-    # empty dataframe to load summarised national daily values into
-    weighted.area.national.total = data.frame()
+# empty dataframe to load summarised national daily values into
+weighted.area.national.total = data.frame()
 
-    # loop through each day of the year and perform analysis
-    print(paste0('Processing dates in ',year))
-    for(date in dates){
+# loop through each day of the year and perform analysis
+print(paste0('Processing dates in ',year))
+for(date in dates){
 
-        print(format(as.Date(date), "%Y-%m-%d"))
+    print(format(as.Date(date), "%Y-%m-%d"))
 
-        # load raster for relevant date
-        raster.full <- raster(paste0('~/data/climate/net_cdf/',dname,'/raw_era5_daily/','worldwide_',dname,'_',freq,'_',num,'_',date,'.nc'))
-        raster.full = projectRaster(raster.full, crs=original.proj) # ERROR HERE WHY???
+    # load raster for relevant date
+    raster.full <- raster(paste0('~/data/climate/net_cdf/',dname,'/raw_era5_daily/','worldwide_',dname,'_',freq,'_',num,'_',date,'.nc'))
+    raster.full = projectRaster(raster.full, crs=original.proj) # ERROR HERE WHY???
 
-        # create empty dataframe to fill with zip code summary information
-        weighted.area.national = data.frame()
+    # flatten the raster's x values per day TO FINISH
+    # raster.full <- calc(raster.full, fun = mean)
 
-        # perform loop across all states
-        system.time(
-        analysis.dummy = uk.analysis(uk.national,raster.full)
-        analysis.dummy$date = format(as.Date(date), "%d/%m/%Y")
-        analysis.dummy$day = day
-        analysis.dummy$month = month
-        analysis.dummy$year = year
+    # create empty dataframe to fill with zip code summary information
+    weighted.area.national = data.frame()
 
-        weighted.area.national = rbind(weighted.area.national,analysis.dummy)
-        }
-        )
+    # perform loop across all states
+    system.time(
+    analysis.dummy = uk.analysis(uk.national,raster.full)
+    analysis.dummy$date = format(as.Date(date), "%d/%m/%Y")
+    analysis.dummy$day = day
+    analysis.dummy$month = month
+    analysis.dummy$year = year
 
-        # weighted.area.national = weighted.area.national[,c(3,1,2)]
-        weighted.area.national.total = rbind(weighted.area.national.total,weighted.area.national)
+    weighted.area.national = rbind(weighted.area.national,analysis.dummy)
     }
+    )
+
+    # weighted.area.national = weighted.area.national[,c(3,1,2)]
+    weighted.area.national.total = rbind(weighted.area.national.total,weighted.area.national)
 }
+
+# save file
+# dir.output = ???
+saveRDS(weighted.area.national.total,paste0(dir.output,'weighted_area_raster_lads_',dname,'_',freq,'_',as.character(year),'.rds'))
