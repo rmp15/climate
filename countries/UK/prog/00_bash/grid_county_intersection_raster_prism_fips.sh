6f12bb9d64b48271afb35d22ca2b1a704d8c401c
rmp15 <r.parks15@imperial.ac.uk>
2020-04-14 08:22:19 -0400

    covid 19 work

--- countries/UK/prog/00_bash/grid_county_intersection_raster_prism_fips.sh
+++ countries/UK/prog/00_bash/grid_county_intersection_raster_prism_fips.sh
@@ -8,6 +8,7 @@ clear
 declare -a years=($(seq 2010 2019))
 declare -a dnames=("t2m")
 declare -a times=("daily")
+declare -a nums=("four")
 declare -a spaces=("lad")
 
 #################################################
@@ -17,35 +18,15 @@ declare -a spaces=("lad")
 for year in "${years[@]}"; do
 for dname in "${dnames[@]}"; do
 for time in "${times[@]}"; do
+for num in "${nums[@]}"; do
 for space in "${spaces[@]}"; do
-for state in "${states[@]}"; do
 
-echo "Identifying overlap between rasters and zip codes for temperature processing";
+echo "Identifying overlap between rasters and LADs codes for temperature processing";
 echo $year;
 
 :
-# identifies the overlap between grids and zip codes and creates weighted means
-#Rscript ~/git/pollution/countries/USA/prog/02_grid_county_intersection/grid_county_intersection_raster_prism.R $year $dname $time $space &
-
-echo "Hello";
-
-done; done; done; done; done;
-
-#################################################
-# 2. SANITY CHECK
-#################################################
-
-for year in "${years[@]}"; do
-for dname in "${dnames[@]}"; do
-for time in "${times[@]}"; do
-for space in "${spaces[@]}"; do
-for state in "${states[@]}"; do
-
-echo "Sanity check of values by plotting";
-echo $year;
-:
-# identifies the overlap between grids and zip codes and creates weighted means
-Rscript ~/git/pollution/countries/USA/prog/03_compare/check_prism.R $year $dname $time $space $state &
+# identifies the overlap between grids and LADs codes and creates weighted means
+Rscript ~/git/climate/countries/UK/prog/01_extract_netcdf/extrac $year $dname $time $num $space &
 
 echo "Hello";
 
