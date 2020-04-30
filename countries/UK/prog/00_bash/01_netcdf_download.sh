5c14d7fe165006bd80930e8d471cfba2a4406ffa
Robbie M Parks <rmp15@wrfproc1-10.sp.ph.ic.ac.uk>
2020-04-30 19:03:47 +0100

    added stuff

---
+++ countries/UK/prog/00_bash/01_netcdf_download.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# this script
+# downloads netcdf files
+
+clear
+
+declare -i start_day='01'
+declare -i start_month='04'
+declare -i start_year='2020'
+declare -i end_day='23'
+declare -i end_month='04'
+declare -i end_year='2020'
+declare -a dnames=("t2m")
+
+
+python ~/git/climate/countries/UK/prog/01_extract_netcdf/downloading_netcdf_files_uk_day_sep_era5_cdsapi.py $start_day $start_month $start_year $end_day $end_month $end_year 't2m';
---
+++ countries/UK/prog/00_bash/03_anomaly_lads.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+# this script
+# identifies overlap between counties and temperature grids and outputs county-level summaries
+
+clear
+
+declare -a dnames=("t2m")
+declare -a times=("daily")
+declare -a nums=("four")
+declare -a spaces=("lad")
+
+#################################################
+# 1. WEIGHTED MEAN CALCULATION
+#################################################
+
+for dname in "${dnames[@]}"; do
+for time in "${times[@]}"; do
+for num in "${nums[@]}"; do
+for space in "${spaces[@]}"; do
+
+echo "Calculating anomalies by LAD for week or month scale";
+
+:
+# identifies the overlap between grids and LADs codes and creates weighted means
+Rscript ~/git/climate/countries/UK/prog/02_anomaly_lads/anomaly_lads_daily.R 2010 2020 $dname $time $num $space &
+
+done; done; done; done; done;
