68082aa38718a1331b0887580500121c7e786aa1
rmp15 <r.parks15@imperial.ac.uk>
2020-05-08 13:30:38 -0400

    covid 19 work

--- countries/Worldwide/prog/01_extract_netcdf/extracting_netcdf_files_era5.R
+++ countries/Worldwide/prog/01_extract_netcdf/extracting_netcdf_files_era5.R
@@ -43,8 +43,9 @@ print(paste0('running extracting_netcdf_files.R for ',country.id,' ',space.res,'
 
 # perform analysis across every day of selected year
 # loop through each raster file for each day and summarise
-if(year<2020){dates <- seq(as.Date(paste0('0101',year),format="%d%m%Y"), as.Date(paste0('3112',year),format="%d%m%Y"), by=1)}
-if(year==2020){dates <- seq(as.Date(paste0('0101',year),format="%d%m%Y"), as.Date(paste0('0505',year),format="%d%m%Y"), by=1)}
+# if(year<2020){dates <- seq(as.Date(paste0('0101',year),format="%d%m%Y"), as.Date(paste0('3112',year),format="%d%m%Y"), by=1)}
+# if(year==2020){dates <- seq(as.Date(paste0('0101',year),format="%d%m%Y"), as.Date(paste0('0505',year),format="%d%m%Y"), by=1)}
+dates = seq(as.Date(paste0('0101',year),format="%d%m%Y"), as.Date(paste0('3112',year),format="%d%m%Y"), by=1)
 dates = as.character(dates)
 
 # function to perform analysis for entire country
@@ -61,6 +62,7 @@ country.analysis = function(shapefile,raster.input,output=0) {
     # convert to centigrade
     weighted.area$layer = round((weighted.area$layer - 273.15),2)
     names(weighted.area) = c(paste0('ID_',space.res),dname)
+
     return(weighted.area)
 
 }
@@ -70,33 +72,41 @@ weighted.area.national.total = data.frame()
 
 # loop through each day of the year and perform analysis
 print(paste0('Processing dates in ',year))
-# for(date in dates){
-for(date in dates[1:10]){
-
-    print(as.character(date))
+for(date in dates){
 
     # load raster for relevant date and change co-ordinates to -180 to 180
-    raster.full <- raster(paste0('~/data/climate/net_cdf/',dname,'/raw_era5_daily/','worldwide_',dname,'_',freq,'_',num,'_',as.character(date),'.nc'))
-    raster.full <- rotate(raster.full)
+    raster.current = paste0('~/data/climate/net_cdf/',dname,'/raw_era5_daily/','worldwide_',dname,'_',freq,'_',num,'_',as.character(date),'.nc')
 
-    # projet to be the same as the chosen country map
-    raster.full = projectRaster(raster.full, crs=original.proj)
+    if(file.exists(raster.current)){
 
-    # flatten the raster's x values per day
-    raster.full <- calc(raster.full, fun = mean)
+        print(as.character(date))
 
-    # plot(raster.full)
+        raster.full <- raster(raster.current)
+        raster.full <- rotate(raster.full)
 
-    # create empty dataframe to fill with zip code summary information
-    weighted.area.national = data.frame()
+        # projet to be the same as the chosen country map
+        raster.full = projectRaster(raster.full, crs=original.proj)
 
-    # perform analysis
-    analysis.dummy =  country.analysis(shapefile,raster.full)
-    analysis.dummy$date = format(as.Date(date), "%Y-%m-%d")
-    weighted.area.national = rbind(weighted.area.national,analysis.dummy)
+        # flatten the raster's x values per day
+        raster.full <- calc(raster.full, fun = mean)
 
-    weighted.area.national = weighted.area.national[,c(3,1,2)]
-    weighted.area.national.total = rbind(weighted.area.national.total,weighted.area.national)
+        # for testing only
+        # plot(raster.full)
+
+        # create empty dataframe to fill with zip code summary information
+        weighted.area.national = data.frame()
+
+        # perform analysis
+        analysis.dummy =  country.analysis(shapefile,raster.full)
+        analysis.dummy$date = format(as.Date(date), "%Y-%m-%d")
+        weighted.area.national = rbind(weighted.area.national,analysis.dummy)
+
+        weighted.area.national = weighted.area.national[,c(3,1,2)]
+        weighted.area.national.total = rbind(weighted.area.national.total,weighted.area.national)
+    }
+    if(!(file.exists(raster.current))){
+        print(paste0(as.character(date),' : file not found'))
+    }
 }
 
 
