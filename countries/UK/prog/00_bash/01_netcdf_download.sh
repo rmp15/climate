71d357364440ceb2d711df9f07ad0d77d1e7573a
rmp15 <r.parks15@imperial.ac.uk>
2020-05-12 07:07:06 -0400

    covid 19 work

--- countries/UK/prog/00_bash/01_netcdf_download.sh
+++ countries/UK/prog/00_bash/01_netcdf_download.sh
@@ -5,8 +5,8 @@
 
 clear
 
-declare -i start_day='04'
-declare -i start_month='05'
+declare -i start_day='01'
+declare -i start_month='04'
 declare -i start_year='2020'
 declare -i end_day='08'
 declare -i end_month='05'
@@ -15,3 +15,4 @@ declare -a dnames=("t2m")
 
 
 python ~/git/climate/countries/UK/prog/01_extract_netcdf/downloading_netcdf_files_uk_day_sep_era5_cdsapi.py $start_day $start_month $start_year $end_day $end_month $end_year 't2m';
+
