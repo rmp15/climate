2a0e30b06d815d80a6d5e9de9224d5ab3b410a8a
rmp15 <r.parks15@imperial.ac.uk>
2020-05-21 16:27:33 -0400

    covid 19 work

--- countries/Worldwide/prog/04_population_weighted_mean/population_weighted_mean.R
+++ countries/Worldwide/prog/04_population_weighted_mean/population_weighted_mean.R
@@ -24,10 +24,24 @@ freq <- as.character(args[4])
 num <- as.character(args[5])
 space.res <- as.character(args[6])
 country.id <- as.character(args[7])
+year.pop = as.character(args[8])
 
 # for code testing
-# dname = 't2m' ; freq = 'daily' ; num = 'four' ; year_start = '2010' ; year_end = '2020' ; space.res='0' ; country.id = 'BEL'
+# dname = 't2m' ; freq = 'daily' ; num = 'four' ; year_start = '2011' ; year_end = '2020' ; space.res='1' ; country.id = 'BEL' ; year.pop = '2010'
 
 years = c(year_start:year_end)
 
-# attach
+# load processed daily anomalies
+dir.input = paste0("../../output/grid_county_intersection_raster/",country.id,'/adm',space.res,'/')
+dat.adm.anomaly = read.csv(paste0(dir.input,'full_weekly_anomalies_weighted_area_raster_adm_',dname,'_',freq,'_',as.character(year_start),'_',as.character(year_end),'.csv'))
+
+# load populalation weightings
+dir.output = paste0("../../output/population_grid_summary/",country.id,'/adm',space.res,'/')
+weighted.area.national = read.csv(paste0(dir.output,'population_',country.id,'_',space.res,'_',as.character(year.pop),'.csv'))
+names(weighted.area.national)[3] = 'population'
+
+# merge population values with temperature records
+dat.merged = merge(dat.adm.anomaly, weighted.area.national, by='ID_1',all.x=TRUE)
+
+# create national means of absolute values as well as anomaly values, weighted by population of subnational regions
+dat.weighted.mean = ddply(dat.merged, .(date), summarize, t2m.weighted=weighted.mean(t2m,population))
