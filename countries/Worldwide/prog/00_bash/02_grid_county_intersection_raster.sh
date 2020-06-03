107a8e2d25792a11dea2a2fb5de3b1d1d6b2aca3
rmp15 <r.parks15@imperial.ac.uk>
2020-06-03 15:45:12 -0400

    covid 19 work

--- countries/Worldwide/prog/00_bash/02_grid_county_intersection_raster.sh
+++ countries/Worldwide/prog/00_bash/02_grid_county_intersection_raster.sh
@@ -8,8 +8,15 @@
 clear
 
 # arguments for running script
-declare -a countries=("ESP" "SWE" "ISL" "BEL" "FRA" "NOR" "DNK" "AUT" "NLD" "ITA" "CAN" "GBR")
-declare -a years=($(seq 2020 2020))
+
+# if updating 2020
+#declare -a countries=("ESP" "SWE" "ISL" "BEL" "FRA" "NOR" "DNK" "AUT" "NLD" "ITA" "CAN" "GBR")
+#declare -a years=($(seq 2020 2020))
+
+#if new country and need 2010 to 2019
+declare -a countries=("SWE")
+declare -a years=($(seq 2010 2019))
+
 declare -a dnames=("t2m")
 declare -a times=("daily")
 declare -a nums=("four")
