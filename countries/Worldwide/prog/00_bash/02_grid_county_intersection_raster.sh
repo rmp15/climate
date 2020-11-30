e8d47d811f83c07f3ea7a2a1a41c5c943bad0cc0
rmp15 <r.parks15@imperial.ac.uk>
2020-11-30 15:37:42 -0500

    Added new dates

--- countries/Worldwide/prog/00_bash/02_grid_county_intersection_raster.sh
+++ countries/Worldwide/prog/00_bash/02_grid_county_intersection_raster.sh
@@ -10,12 +10,12 @@ clear
 # arguments for running script
 
 # if updating 2020
-#declare -a countries=("NZL" "ESP" "SWE" "ISL" "BEL" "FRA" "NOR" "DNK" "AUT" "NLD" "ITA" "CAN" "GBR" "BGR" "CZE" "FIN" "HUN" "PRT" "CHE" "POL" "SVK" "AUS" "GRC" "ROU" "USA" "DEU")
-#declare -a years=($(seq 2020 2020))
+declare -a countries=("NZL" "ESP" "SWE" "ISL" "BEL" "FRA" "NOR" "DNK" "AUT" "NLD" "ITA" "CAN" "GBR" "BGR" "CZE" "FIN" "HUN" "PRT" "CHE" "POL" "SVK" "AUS" "GRC" "ROU" "USA" "DEU")
+declare -a years=($(seq 2020 2020))
 
 # if new country and need 2010 to 2019
-declare -a countries=("USA")
-declare -a years=($(seq 2010 2019))
+#declare -a countries=("USA")
+#declare -a years=($(seq 2010 2019))
 
 # For Africa children hospital study (by default should be commented out
 #declare -a countries=("AFR")
@@ -25,8 +25,8 @@ declare -a years=($(seq 2010 2019))
 declare -a dnames=("t2m")
 declare -a times=("daily")
 declare -a nums=("four")
-declare -a admin_levels=("0" "1")
-#declare -a admin_levels=("2")
+declare -a admin_levels=("1")
+#declare -a admin_levels=("0" "2")
 
 #################################################
 # WEIGHTED MEAN CALCULATION
